7041bc561c61293e00572235340f5ba61bd6d398
Aurélien LAJOIE <orel@melix.net>
2015-04-06 11:39:23 +1000

    Add fans status and warning critical levels

--- plugins/dns320_temp.sh
+++ plugins/dns320_temp.sh
@@ -13,6 +13,8 @@ Any DNS 320 with funplug installed (should work with other DNS like 323 not test
 =head1 CONFIGURATION
 
 This plugin needs root privileges
+Set WARNING and CRITICAL directly into the script
+
 
 =head1 INTERPRETATION
 
@@ -27,6 +29,14 @@ Copyright (C) 2015 Aurelien LAJOIE, Utix
 GPLv2
 
 =cut
+# define warning and critical temperature levels
+CASE_WARNING="50"
+CASE_CRITICAL="60"
+DISK_WARNING="45"
+DISK_CRITICAL="50"
+FAN_STOP="30"
+FAN_ROTATE="35"
+
 
 case $1 in
 config)
@@ -39,13 +49,29 @@ graph_order system hda hdb
 system.label System
 hda.label Disk A
 hdb.label Disk B
+fan.label Is Fan rotating
 EOM
+echo "system.warning $CASE_WARNING"
+echo "system.critical $CASE_CRITICAL"
+echo "hda.warning $DISK_WARNING"
+echo "hda.critical $DISK_CRITICAL"
+echo "hdb.warning $DISK_WARNING"
+echo "hdb.critical $DISK_CRITICAL"
 exit 0;;
 esac
 
 Ta=`smartctl -d marvell --all /dev/sda |grep -e ^194 | head -c 40 | tail -c 2`
 Tb=`smartctl -d marvell --all /dev/sdb |grep -e ^194 | head -c 40 | tail -c 2`
 ST=`FT_testing -T | tail -c 3 | head -c 2`
+case `fanspeed g` in
+    stop)
+    Fan=$FAN_STOP
+    ;;
+    *)
+    Fan=$FAN_ROTATE
+    ;;
+esac
 echo  "system.value"  $ST
 echo  "hda.value"  $Ta
 echo  "hdb.value"  $Tb
+echo  "fan.value" $Fan
